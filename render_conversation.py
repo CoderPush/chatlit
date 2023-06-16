8cbea229c1f69c3b6b774db36a332b95f9cb275f
NghiaDinh003 <92510310055@ispace.edu.vn>
2023-06-16 14:23:48 +0700

    New pase setting edit cÃustom thÃeme

--- render_conversation.py
+++ render_conversation.py
@@ -1,23 +1,21 @@
 from firestore_utils import get_firestore_db
-from streamlit_chat import message as chat_message
+import streamlit as st
 
 
 def render_messages(st, messages):
     user_picture_url = st.session_state.get("user_info", {}).get("picture", None)
     for i, message in enumerate(messages):
         if message.get("role") == "user":
-            chat_message(
-                message.get("content"),
+            st.text_area(
+                label="User Message",
+                value=message.get("content"),
                 key=str(i) + "_user",
-                is_user=True,
-                logo=user_picture_url,
             )
         elif message.get("role") == "assistant":
-            chat_message(
+            st.markdown(
                 message.get("content"),
                 key=str(i) + "_assistant",
-                allow_html=True,
-                is_table=True,
+                unsafe_allow_html=True,
             )
         elif message.get("role") == "system":
             # don't render system messages
