07779365f4a16e64e25a66a4f3f316f7a01c1520
linh <linh.ha.nguyen20@gmail.com>
2023-06-19 13:19:57 +0700

    remove st-chat fromp pipfile. remove pl_tags from ChatCompletion

--- render_conversation.py
+++ render_conversation.py
@@ -4,9 +4,9 @@ from firestore_utils import get_firestore_db
 def render_messages(st, messages):
     for i, message in enumerate(messages):
         if message.get("role") == "user":
-            st.warning(message.get("content"))
+            st.warning(message.get("content"), icon="👤")
         elif message.get("role") == "assistant":
-            st.info(message.get("content"))
+            st.info(message.get("content"), icon="🤖")
         elif message.get("role") == "system":
             # don't render system messages
             pass
