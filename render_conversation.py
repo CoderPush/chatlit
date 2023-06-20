7d77809380f6f5be2167bbc9379fc9f6560f3a9c
linh <linh.ha.nguyen20@gmail.com>
2023-06-20 14:56:05 +0700

    add custom html for user message

--- render_conversation.py
+++ render_conversation.py
@@ -1,10 +1,24 @@
 from firestore_utils import get_firestore_db
 
 
+def render_user_message(st, message):
+    user_message = f"""
+        <div style="background-color: rgba(255, 227, 18, 0.2); \
+                    color: rgb(255, 255, 194);
+                    padding: 16px; \
+                    border-radius: 0.25rem; \
+                    line-height: 1.6; \
+                    font-size: 1rem;" \
+            >{message}</div>
+    """
+    st.markdown(user_message, unsafe_allow_html=True)
+
 def render_messages(st, messages):
     for i, message in enumerate(messages):
+        message_content = message.get("content")
         if message.get("role") == "user":
             st.warning(message.get("content"), icon="👤")
+            render_user_message(st, message_content)
         elif message.get("role") == "assistant":
             st.info(message.get("content"), icon="🤖")
         elif message.get("role") == "system":
