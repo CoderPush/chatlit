25543aac81618494f5a4c7841d21c9a04a4b277b
linh <linh.ha.nguyen20@gmail.com>
2023-06-21 16:58:39 +0700

    remove reference chat message

--- render_conversation.py
+++ render_conversation.py
@@ -2,7 +2,6 @@ from firestore_utils import get_firestore_db
 
 
 def render_user_message(st, message, profile_url):
-    # .css-nahz7x
     user_message = f"""
         <div role="alert" data-baseweb="notification" class="st-ae st-af st-ag st-ah st-ai st-aj st-ak st-ee st-am st-ed st-an st-ao st-ap st-aq st-ar st-as st-ef st-au st-av st-aw st-ax st-ay st-bb st-b0 st-b1 st-b2 st-b3 st-b4 st-b5 st-b6 st-b7"><div class="st-b8 st-b9"><div class="css-17z2rne e13vu3m50"><div class="css-1w6rlcb e12sa3f30">
             <img src={profile_url} width=32 height=32 alt="Profile Picture">
@@ -18,7 +17,6 @@ def render_messages(st, messages):
     for i, message in enumerate(messages):
         message_content = message.get("content")
         if message.get("role") == "user":
-            st.warning(message_content, icon="👤")
             render_user_message(st, message_content, user_picture_url)
         elif message.get("role") == "assistant":
             st.info(message.get("content"), icon="🤖")
