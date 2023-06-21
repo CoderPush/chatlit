4ed5c25e603092a55aa71898ac19d0808d7f030f
linh <linh.ha.nguyen20@gmail.com>
2023-06-21 15:24:06 +0700

    embed profile using img html tag

--- render_conversation.py
+++ render_conversation.py
@@ -1,24 +1,25 @@
 from firestore_utils import get_firestore_db
 
 
-def render_user_message(st, message):
+def render_user_message(st, message, profile_url):
+    # .css-nahz7x
     user_message = f"""
-        <div style="background-color: rgba(255, 227, 18, 0.2); \
-                    color: rgb(255, 255, 194);
-                    padding: 16px; \
-                    border-radius: 0.25rem; \
-                    line-height: 1.6; \
-                    font-size: 1rem;" \
-            >{message}</div>
+        <div role="alert" data-baseweb="notification" class="st-ae st-af st-ag st-ah st-ai st-aj st-ak st-ee st-am st-ed st-an st-ao st-ap st-aq st-ar st-as st-ef st-au st-av st-aw st-ax st-ay st-bb st-b0 st-b1 st-b2 st-b3 st-b4 st-b5 st-b6 st-b7"><div class="st-b8 st-b9"><div class="css-17z2rne e13vu3m50"><div class="css-1w6rlcb e12sa3f30">
+            <img src={profile_url} width=32 height=32 alt="Profile Picture">
+            <div data-testid="stMarkdownContainer" class="css-nahz7x e16nr0p34">
+                <p>{message}</p>
+            </div>
+        </div></div></div></div>
     """
     st.markdown(user_message, unsafe_allow_html=True)
 
 def render_messages(st, messages):
+    user_picture_url = st.session_state.get("user_info", {}).get("picture", None)
     for i, message in enumerate(messages):
         message_content = message.get("content")
         if message.get("role") == "user":
-            st.warning(message.get("content"), icon="👤")
-            render_user_message(st, message_content)
+            st.warning(message_content, icon="👤")
+            render_user_message(st, message_content, user_picture_url)
         elif message.get("role") == "assistant":
             st.info(message.get("content"), icon="🤖")
         elif message.get("role") == "system":
