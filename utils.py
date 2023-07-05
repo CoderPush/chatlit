239117a297a2912a2fdf9e4a19fe6cfb4373ef2e
Huyen Nguyen <65232736+huyenNguyen20@users.noreply.github.com>
2023-07-05 10:03:57 +0700

    Revert "fix/ not working conversation btn in the history list" (#40)
    Reverts CoderPush/chatlit#39

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 from firestore_utils import delete_convo, edit_convo
 from custom_js import render_copy_shared_convo_link
-from constants import DEFAULT_CONVERSATION
 
 
 def link_button(st, text, path):
@@ -41,13 +40,9 @@ def button_row(st, cid, conversation, selected=False):
     title = conversation.get("title", cid)
     container = st.sidebar.container()
 
-    if "title_button" not in st.session_state:
-        st.session_state["title_button"] = {}
-    if cid not in st.session_state["title_button"]:
-        st.session_state["title_button"][cid] = False
-
     with container:
         col1, col2, col3, col4 = st.columns([6, 1, 1, 1], gap="small")
+        is_edit_mode = st.session_state.get(f"title_button_{cid}", False)
         is_edit = st.session_state.get(f"edit_convo_button_{cid}", False)
 
         with col1:
@@ -64,17 +59,10 @@ def button_row(st, cid, conversation, selected=False):
             else:
                 convo_button = st.button(
                     title,
-                    key=f"title_{cid}",
+                    key=f"title_button_{cid}",
                     disabled=selected,
                     use_container_width=True,
                 )
-                if convo_button:
-                    for key in st.session_state["title_button"]:
-                        st.session_state["title_button"][key] = False
-
-                    st.session_state["title_button"][cid] = True
-                    st.session_state["cid"] = cid
-                    st.experimental_rerun()
 
             new_title = st.session_state.get(f"new_title_{cid}", "")
             if new_title and new_title != title:
@@ -82,7 +70,7 @@ def button_row(st, cid, conversation, selected=False):
                 st.session_state[f"new_title_{cid}"] = ""
                 st.experimental_rerun()
 
-        if st.session_state["title_button"][cid]:
+        if is_edit_mode:
             with col2:
                 st.button(
                     ":outbox_tray:",
@@ -96,7 +84,7 @@ def button_row(st, cid, conversation, selected=False):
                 st.button(
                     ":pencil2:",
                     key=f"edit_convo_button_{cid}",
-                    disabled=False,
+                    disabled=selected,
                     use_container_width=True,
                 )
 
@@ -104,13 +92,12 @@ def button_row(st, cid, conversation, selected=False):
                 delete_button = st.button(
                     ":wastebasket:",
                     key=f"delete_convo_button_{cid}",
-                    disabled=False,
+                    disabled=selected,
                     use_container_width=True,
                 )
-                if delete_button:
+                is_delete = st.session_state.get(f"delete_convo_button_{cid}", False)
+                if is_delete:
                     delete_convo(cid)
-                    st.session_state["cid"] = None
-                    st.session_state["conversation"] = DEFAULT_CONVERSATION
                     st.experimental_rerun()
         else:
             with col4:
